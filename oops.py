53b354d74fcfaeaad4143d4673e76a86ffcf269a
RajeshAbhimanyu <rajeshthokala10@gmail.com>
2020-11-18 00:17:32 +0530

    18 Nov 2020 -1 oops

---
+++ oops.py
@@ -0,0 +1,9 @@
+#constructors
+class TestConstructor:
+    def __init__ (self):
+        self.name="Rajesh"
+    #method for printing data members
+    def printName(self):
+        print(self.name)
+obj=TestConstructor()
+obj.printName()
