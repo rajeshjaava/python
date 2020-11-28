5e62700cf3caec416591ac1727610b6d985f13e8
RajeshAbhimanyu <rajeshthokala10@gmail.com>
2020-11-28 23:11:35 +0530

    28 Nov 2020 -1

---
+++ Dict.py
@@ -0,0 +1,8 @@
+dict={"name":"rajesh","course":"learning python"}
+for k,v in dict.items():
+    print(k,v)
+q=["what is your name .?","How old are you.?","What's your education background .?"]
+a=["Rajesh","29","MCA"]
+for x,v in zip(q,a):
+    print(x,v)
+
